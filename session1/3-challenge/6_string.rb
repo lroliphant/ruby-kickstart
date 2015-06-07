78c563f8fd163abb7cec0c7efa2c861509bd9d79
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-07 23:07:44 +0100

    Add session1 exercises

--- session1/3-challenge/6_string.rb
+++ session1/3-challenge/6_string.rb
@@ -1,10 +1,37 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-#
+# 
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-#
+# 
+# odds_and_evens("abcdefg",true)    # => "bdf"
+# odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
-
+  str_a = string.each_char.to_a
+  
+  if return_odds == true
+    odds = str_a.values_at(* str_a.each_index.select {|i| i.odd?})
+    puts odds.to_s
+  else
+    evens = str_a.values_at(* str_a.each_index.select {|i| i.even?})
+    puts evens.to_s
+  end
 end
+
+odds_and_evens("abcdefg",true)
+odds_and_evens("abcdefg",false)
+
+
+
+# Solution
+# 
+# def odds_and_evens(string, return_odds)
+#   to_return = ""
+#   string.size.times do |index|
+#     next if return_odds && index.even?
+#     next if !return_odds && index.odd?
+#     to_return << string[index]
+#   end
+#   to_return
+# end
--- session1/3-challenge/7_string.rb
+++ session1/3-challenge/7_string.rb
@@ -1,9 +1,28 @@
 # given a string, return the character after every letter "r"
-#
+# 
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
-def pirates_say_arrrrrrrrr(string)
 
+def pirates_say_arrrrrrrrr(string)
+  return_str = ""
+  
+  add_next = false
+  
+  string.size.times do |index|
+    
+    current_char = string[index] 
+    
+    return_str << current_char if add_next
+    
+    add_next = (current_char == "r" || current_char == "R")
+  end
+
+  to_return
 end
+
+
+
+
+pirates_say_arrrrrrrrr("are you really learning Ruby?")
